d0725970f6a9aa80ac444d4ddd25eae147e27801
Luc Van Den Handel <vhnluc001@cs.uct.ac.za>
2021-08-16 19:21:45 +0200

    added a trial for reading password from a text file

--- app.py
+++ app.py
@@ -14,8 +14,22 @@ def test():
     data = request.get_json()
     username = data.get('username', '')
     password = data.get('password', '')
-    #print(username, password)
+    # print(username, password)
     if username == "test" and password == "123":
+        access = "granted!"
+    return access
+
+
+@app.route("/signin", methods=['GET'])
+def signin():
+    access = "denied!"
+    data = request.get_json()
+    usernameIn = data.get('username', '')
+    passwordIn = data.get('password', '')
+    file = open("usrs/" + usernameIn + ".txt")
+    passwordS = file.readline().strip()
+    print(passwordS)
+    if passwordIn == passwordS:
         access = "granted!"
     return access
 
