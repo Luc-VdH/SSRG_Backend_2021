b02723edb55814f095eda29efab452e2017f4f10
Luc Van Den Handel <vhnluc001@cs.uct.ac.za>
2021-08-16 18:30:04 +0200

    added a simple test to try sending data as json in the body of the http request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return "Hello World"
+
+
+@app.route("/test")
+def test():
+    access = "denied!"
+    data = request.get_json()
+    username = data.get('username', '')
+    password = data.get('password', '')
+    #print(username, password)
+    if username == "test" and password == "123":
+        access = "granted!"
+    return access
 
 
 if __name__ == "__main__":
