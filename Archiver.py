d16cf7752cf34316d5e7daefa1fd80eb5d2dee55
Luc Van Den Handel <vhnluc001@cs.uct.ac.za>
2021-08-24 17:48:16 +0200

    shell things

--- Archiver.py
+++ Archiver.py
@@ -11,7 +11,7 @@ class Archiver:
         for file in files:
             filename, file_extension = os.path.splitext(file)
             if file_extension == ".zip":
-                os.system("unzip " + file)
+                os.system("unzip " + file.split("/")[-1])
                 # for f in os.listdir(filename): # go through files in archive
                 #     studentfolder = os.path.join(filename, f)
                 #     if not os.path.isfile(studentfolder):
