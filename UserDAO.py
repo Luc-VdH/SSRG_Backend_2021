f3445e1084f846f167fdf0ead0e839c93a39c381
Luc Van Den Handel <vhnluc001@cs.uct.ac.za>
2021-08-24 09:47:08 +0200

    renamed user class to start with capital letter, created empty user DAO class

---
+++ UserDAO.py
@@ -0,0 +1,20 @@
+from User import User
+
+
+class UserDAO:
+    __users = []
+
+    def __init__(self):
+        pass
+
+    def addUser(self):
+        pass
+
+    def signIn(self):
+        pass
+
+    def deleteUser(self):
+        pass
+
+    def updateUserInfo(self):
+        pass
