447f323c3188512994b1415f0381ab82b1eba43b
Luc Van Den Handel <vhnluc001@cs.uct.ac.za>
2021-09-09 11:38:35 +0200

    changed match to use json dumps instead of string manipulation

--- Match.py
+++ Match.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Match:
     def __init__(self, file1, file2, percent):
         self.__file1 = file1
@@ -11,21 +14,23 @@ class Match:
             self.__lines.remove([])
 
     def toString(self):
-        out = '{"files": ["' + self.__file1 + '", "' + self.__file2 + '"],'
-        out += '"percent": "' + self.__percent + '", "lines": ['
-        for line in self.__lines:
-            out += '["'
-            out += line[0].replace('\n', '\\n').replace('"', '\\"') + '", "' + line[1].replace('\n', '\\n').replace('"', '\\"')
-            out += '"], '
-        out = out[:-2]
-        out += ']}'
+        # out = '{"files": ["' + self.__file1 + '", "' + self.__file2 + '"], '
+        # out += '"percent": "' + self.__percent + '", "lines": ['
+        # for line in self.__lines:
+        #     out += '["'
+        #     out += line[0].replace('\n', '\\n').replace('"', '\\"') + '", "' + line[1].replace('\n', '\\n').replace('"', '\\"')
+        #     out += '"], '
+        # out = out[:-2]
+        # out += ']}'
 
-        return out
+        outdict = {"files": [self.__file1, self.__file2], "percent": self.__percent, "lines": self.__lines}
+
+        return json.dumps(outdict)
 
 
 if __name__ == "__main__":
     m = Match("file1", "file2", "20%")
-    m.addLines("line00", "line01")
+    m.addLines("line00    \n\t \"hello\"", "line01")
     m.addLines("line10", "line11")
     m.addLines("line20", "line21")
     print(m.toString())
