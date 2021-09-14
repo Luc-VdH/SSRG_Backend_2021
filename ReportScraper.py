7e4f33f9632bc4481f8c1f1a5ef0644e3f5cfbb8
Luc Van Den Handel <vhnluc001@cs.uct.ac.za>
2021-09-14 15:03:56 +0200

    changed scraper to use requests instead of selenium, also fixed bug when more than 9 blocks of matching code are found

--- ReportScraper.py
+++ ReportScraper.py
@@ -1,5 +1,6 @@
 import urllib.request
 from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
 from Match import Match
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,11 +9,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 class ReportScraper:
     def __init__(self, url):
         self.urlOfRawReport = url
-        print("setting driver...")
-        opts = webdriver.ChromeOptions()
-        opts.headless =True
-        #self.__driver = webdriver.Chrome(ChromeDriverManager().install())
-        self.__driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=opts)#self.__driver = webdriver.Firefox()
+        # print("setting driver...")
+        # opts = webdriver.ChromeOptions()
+        # opts.headless =True
+        # #self.__driver = webdriver.Chrome(ChromeDriverManager().install())
+        # self.__driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver", options=opts)#self.__driver = webdriver.Firefox()
+        # self.__driver = request.get()
         self.__matches = []
 
     def scrapeReport(self):
@@ -22,24 +24,26 @@ class ReportScraper:
         # urllib.request.urlretrieve(urlOfRawReport, path + "rawReport.html")
 
         print("getting webpage...")
-        self.__driver.get(self.urlOfRawReport)
+        # self.__driver.get(self.urlOfRawReport)
+        r = requests.get(self.urlOfRawReport)
 
         print("getting content...")
-        content = self.__driver.page_source
+        # content = self.__driver.page_source
+        content = r.content
         soup = BeautifulSoup(content, features="html.parser")
         print("scraping index...")
         count = 0
         count_mismatch = 0
 
         s = soup.findAll('a', href=True)
-        for a in range(0, len(s) - 1):
+        for a in range(0, len(s) - 1, 2):
             index = s[a].text.find("%")
             if index != -1:
                 href = s[a]['href']
                 file1 = s[a].text[:-6]
                 file2 = s[a + 1].text[:-6]
                 percent = s[a].text[-4:-1]
-                a += 2
+                # a += 2
                 print("scraping match...")
                 count += 1
                 lines = self.scrapeMatch(href)
@@ -59,10 +63,12 @@ class ReportScraper:
     def scrapeMatch(self, url):
 
         print("\tgetting webpage...")
-        self.__driver.get(url)
+        # self.__driver.get(url)
+        r = requests.get(url)
 
         print("\tgetting content...")
-        content = self.__driver.page_source
+        # content = self.__driver.page_source
+        content = r.content
         soup = BeautifulSoup(content, features="html.parser")
         frameTop = soup.find('frame', attrs={'name': 'top'})
         frame0 = soup.find('frame', attrs={'name': '0'})
@@ -70,8 +76,10 @@ class ReportScraper:
         lines = [[], []]
 
         print("\tscrapeing line count...")
-        self.__driver.get(self.urlOfRawReport + "/" + frameTop['src'])
-        content = self.__driver.page_source
+        # self.__driver.get(self.urlOfRawReport + "/" + frameTop['src'])
+        # content = self.__driver.page_source
+        r = requests.get(self.urlOfRawReport + "/" + frameTop['src'])
+        content = r.content
         soup = BeautifulSoup(content, features="html.parser")
         names = []
         for a in soup.findAll('a'):
@@ -85,19 +93,24 @@ class ReportScraper:
             lines[1].append("")
 
         print("\tscraping lines...")
-        self.__driver.get(self.urlOfRawReport + "/" + frame0['src'])
-        content = self.__driver.page_source
+        # self.__driver.get(self.urlOfRawReport + "/" + frame0['src'])
+        # content = self.__driver.page_source
+        r = requests.get(self.urlOfRawReport + "/" + frame0['src'])
+        content = r.content
         soup = BeautifulSoup(content, features="html.parser")
         for f in soup.findAll('font'):
             a = f.find('a')
             h = a['href']
             # TODO what about 2 digit blocks !? split on #
-            block = eval(h[-1:])
+            # block = eval(h[-1:])
+            block = eval(h.split('#')[-1])
             # TODO identify if 2 sets of lines belong to the same block
             lines[0][block] += f.text
 
-        self.__driver.get(self.urlOfRawReport + "/" + frame1['src'])
-        content = self.__driver.page_source
+        # self.__driver.get(self.urlOfRawReport + "/" + frame1['src'])
+        # content = self.__driver.page_source
+        r = requests.get(self.urlOfRawReport + "/" + frame1['src'])
+        content = r.content
         soup = BeautifulSoup(content, features="html.parser")
         for f in soup.findAll('font'):
             a = f.find('a')
@@ -118,8 +131,10 @@ class ReportScraper:
 
 
 if __name__ == "__main__":
-    rs = ReportScraper("http://moss.stanford.edu/results/7/3579624107546/")
+    # rs = ReportScraper("http://moss.stanford.edu/results/7/3579624107546/")
     # rs = ReportScraper("http://moss.stanford.edu/results/2/4286344033639")
     # rs = ReportScraper("http://moss.stanford.edu/results/7/9634195485591")
+
+    rs = ReportScraper(" http://moss.stanford.edu/results/3/4277668591562")
     rs.scrapeReport()
     print(rs.toString())
